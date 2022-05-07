136d8830a01d836370e553cc5fd852bc1c71ab84
DESKTOP-M5FRLUV\kelmp <email@denniskelm.de>
2022-05-07 18:24:38 +0200

    shared dir

---
+++ src/main/java/shared/communication/IDienstleistungsverwaltung.java
@@ -0,0 +1,36 @@
+package shared.communication;
+
+/*
+@author
+TODO Raphael Kleebaum
+TODO Jonny Schlutter
+Gabriel Kleebaum
+TODO Mhd Esmail Kanaan
+TODO Gia Huy Hans Tran
+TODO Ole Björn Adelmann
+TODO Bastian Reichert
+TODO Dennis Kelm
+*/
+
+import server.Dienstleistungsangebot;
+import server.Dienstleistungsgesuch;
+import server.users.Mitglied;
+
+import java.awt.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.time.LocalDateTime;
+
+// Interface, um RMI für Klasse Dienstleistungsverwaltung zu ermöglichen
+public interface IDienstleistungsverwaltung extends Remote {
+
+    void gesuchErstellen(String titel, String beschreibung, String kategorie, Image[] bilder, String ersteller) throws RemoteException;
+    void angebotErstellen(String titel, String beschreibung, String kategorie, LocalDateTime ab, LocalDateTime bis, Mitglied ersteller) throws RemoteException;
+    void gesuchLoeschen(Dienstleistungsgesuch gesuch) throws RemoteException;
+    void angebotLoeschen(Dienstleistungsangebot angebot) throws RemoteException;
+    void gesuchAendern(Dienstleistungsgesuch gesuch, Object attr, Object wert) throws RemoteException;
+    void angebotAendern(Dienstleistungsangebot angebot, Object attr, Object wert) throws RemoteException;
+    void gesuchAnnehmen(Dienstleistungsgesuch gesuch, String ersteller, String nutzer, int stunden) throws RemoteException;
+    void angebotAnfragen(Dienstleistungsangebot angebot, String ersteller, String fragender) throws RemoteException;
+
+}
---
+++ src/main/java/shared/communication/IGeraeteverwaltung.java
@@ -0,0 +1,33 @@
+package shared.communication;
+
+import server.Geraet;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/*
+@author
+TODO Raphael Kleebaum
+TODO Jonny Schlutter
+Gabriel Kleebaum
+TODO Mhd Esmail Kanaan
+TODO Gia Huy Hans Tran
+TODO Ole Björn Adelmann
+TODO Bastian Reichert
+TODO Dennis Kelm
+*/
+
+// Interface, um RMI für Klasse Geraeteverwaltung zu ermöglichen
+public interface IGeraeteverwaltung extends Remote {
+
+    void geraeteVerwaltungLaden() throws RemoteException;
+    void geraetHinzufuegen(String name, String spender, int leihfrist, String kategorie, String beschreibung, String abholort) throws RemoteException;
+    void geraetReservieren(Geraet geraet) throws RemoteException;
+    void geraetAusgeben(Geraet geraet) throws RemoteException;
+    void geraetAnnehmen(Geraet geraet) throws RemoteException;
+    void geraetEntfernen(Geraet geraet) throws RemoteException;
+    void gereateDatenVerwalten(Geraet g, Object attr, Object wert) throws RemoteException;
+    void historieZuruecksetzen(Geraet geraet) throws RemoteException;
+    void geraeteAnzeigen() throws RemoteException;
+
+}
