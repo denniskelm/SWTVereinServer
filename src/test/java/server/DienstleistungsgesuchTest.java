1a7d98e1e14f19eb565e83c3766be56b331a1a06
DESKTOP-M5FRLUV\kelmp <email@denniskelm.de>
2022-05-09 13:12:09 +0200

    test init

---
+++ src/test/java/server/DienstleistungsgesuchTest.java
@@ -0,0 +1,25 @@
+package server;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+@author
+TODO Raphael Kleebaum
+TODO Jonny Schlutter
+TODO Gabriel Kleebaum
+Mhd Esmail Kanaan
+TODO Gia Huy Hans Tran
+TODO Ole Björn Adelmann
+TODO Bastian Reichert
+TODO Dennis Kelm
+*/
+
+//TODO Was macht diese Klasse?
+class DienstleistungsgesuchTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
