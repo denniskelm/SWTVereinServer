1c5175e9342e276b24667a8d43c43f17cc433b9a
gk266 <gabrielkleebaum@yahoo.de>
2022-06-18 18:30:33 +0200

    Fehler in RMI behoben

--- src/main/java/shared/communication/IRollenverwaltung.java
+++ src/main/java/shared/communication/IRollenverwaltung.java
@@ -56,7 +56,7 @@ public interface IRollenverwaltung extends Remote {
     public Object[][] gaesteDaten() throws RemoteException;
     public Object[] gastDaten(String mitgliedsID) throws RemoteException;
     public String mahnungsIdErstellen() throws RemoteException;
-    boolean existiertEMail(String email);
+    boolean existiertEMail(String email) throws RemoteException;
     public Rolle fetchRolle(String mitgliedsID) throws RemoteException;
     public void nutzerAusAlterListeEntfernen(String mitgliedsID, Rolle rolle) throws RemoteException;
 
